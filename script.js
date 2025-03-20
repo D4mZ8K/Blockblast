aa456cd939e7d3dee397c648401d43b596876e55
D4mZ8K <isabel.mork@gmail.com>
2025-03-20 09:23:38 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function checkLogin() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username === "Admin" && password === "4aPTaH110866") {
+        document.getElementById("login-container").style.display = "none";
+        document.getElementById("game-container").style.display = "block";
+        startGame();
+    } else {
+        alert("Wrong fucking username or password hoe fuck you!");
+    }
+}
+
